93b2a4a6022048cd2463841c7f08cb2d19c849ce
leliemenyeram <leliemenye@ramplc.com>
2015-05-01 14:59:56 +0100

    UpdateFri May 01 2015 14:59:51 GMT+0100 (GMT Daylight Time)

--- app/build/app.js
+++ app/build/app.js
@@ -62,7 +62,7 @@ var ngTableDoc = angular.module('ngTableDoc', ['ui.router', 'ngMessages'])
 
 ngTableDoc.constant('NAVSERVICE', NAVSERVICE_CONSTANT.data)
 
-}).call(this,require("/NJiQA"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_886f4b44.js","/")
+}).call(this,require("/NJiQA"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_a19eacbd.js","/")
 },{"./nav-service":2,"/NJiQA":6,"buffer":3}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 module.exports = {
